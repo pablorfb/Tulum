268fcab9edc70a3876c82ae798c668b25aa5dbcd
pabloRuiz <pablo_ruizf@hotmail.com>
2019-03-23 12:52:41 -0700

    Project creation

---
+++ Bussiness/Tulum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tulum.Bussiness
+{
+    public class Tulum
+    {
+
+        public Tulum()
+        {
+
+        }
+    }
+}
---
+++ Models/Interfaces.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tulum.Models
+{
+
+    enum GameStates
+    {
+        Initializing, Running, Finished
+    }
+
+    enum PlayerStates
+    {
+        Waiting, ReadyToStartTurn, InTurn
+    }
+
+    interface ITulum
+    {
+        void StartGame();
+
+        void NotifyTurnStart();
+
+        void NotifyTurnEnd();
+
+    }
+
+    interface IPlayer
+    {
+        PlayerStates GetState();
+
+        void NotifyTurnStart();
+
+        void NotifyTurnEnd();
+
+        bool PresentOffer(Deal offer);
+
+    }
+
+    struct Deal
+    {
+        public [] Requests;
+        object[] Offers;
+    }
+
+    struct
+    {
+        ResourceType
+    }
+
+    enum ResourceTypes {
+        
+    }
+
+
+}
